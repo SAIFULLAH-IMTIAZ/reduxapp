8e6e60318f20c2a59943b3bcec0136daa7813dcb
SAIFULLAH-IMTIAZ <saifullahimtiaz@gmail.com>
2022-04-07 16:07:29 +0500

    upload first files

---
+++ src/reducers/Index.js
@@ -0,0 +1,11 @@
+import changeTheNumber from "./upDown.js";
+
+import { combineReducers } from "redux";
+
+const reducers = combineReducers(
+    {
+        changeTheNumber
+    }
+);
+
+export default reducers;
